44acd4c9bc33f478315951bb2fbec4102040ebc6
Jeff <jeffp2662@gmail.com>
2020-08-08 22:53:19 -0600

    Adding basic form function and CORS

--- api/app.js
+++ api/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const routes = require('./routes');
+const cors = require('cors')
 
 //  Connect all our routes to our application
 app.use('/', routes);
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
